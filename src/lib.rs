330db0e8fcf9411fdf199f6335c85d8443ca2c8d
Herman Ceaser <ceaserbanks09@gmail.com>
2025-01-09 14:10:09 +0300

    enrolled to the program using my private key and github username: -  Added solana-idlgen -  Added borsh - TX Hash: `2yHkCHqW9sTn5Gm2TzAugdhC8KvH2RXF1bNx8yR2KBhgTFDxpCxmw94Bs6E7MvuWwgjJT6RioLp8UULgMFsFgBkp`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod programs;
+
 #[cfg(test)]
 mod tests {
     use bs58;
@@ -6,12 +8,15 @@ mod tests {
     use solana_sdk::{
         message::Message,
         signature::{read_keypair_file, Keypair, Signer},
+        system_program,
         transaction::Transaction,
     };
     use std::{
         io::{self, BufRead},
         str::FromStr,
     };
+
+    use crate::programs::turbin3_prereq::{CompleteArgs, Turbin3PrereqProgram};
 
     const RPC_URL: &str = "https://api.devnet.solana.com";
 
@@ -123,5 +128,37 @@ mod tests {
             "Success! Check out your TX here: https://explorer.solana.com/tx/{}/?cluster=devnet",
             signature
         )
+    }
+
+    #[test]
+    fn enroll() {
+        let rpc_client = RpcClient::new(RPC_URL);
+        let signer = read_keypair_file("Turbin3-wallet.json").expect("Couldn't find wallet file!");
+
+        let prereq = Turbin3PrereqProgram::derive_program_address(&[
+            b"prereq",
+            signer.pubkey().to_bytes().as_ref(),
+        ]);
+        let args = CompleteArgs {
+            github: b"HermanCeaser".to_vec(),
+        };
+
+        let blockhash = rpc_client
+            .get_latest_blockhash()
+            .expect("Failed to get recent blockhash");
+
+        let transaction = Turbin3PrereqProgram::complete(
+            &[&signer.pubkey(), &prereq, &system_program::id()],
+            &args,
+            Some(&signer.pubkey()),
+            &[&signer],
+            blockhash,
+        );
+
+        let signature = rpc_client
+            .send_and_confirm_transaction(&transaction)
+            .expect("Failed to send the transaction");
+
+        println!("Success! check out your TX here: https://explorer.solana.com/tx/{}/?cluster=devnet", signature)
     }
 }
