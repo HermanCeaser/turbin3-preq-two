71b0c88dde23117cd9ec5a4513a612b0e6c380f6
Herman Ceaser <ceaserbanks09@gmail.com>
2025-01-09 12:20:00 +0300

    added convienience functions to convert inbetween wallet formats

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
 #[cfg(test)]
 mod tests {
+    use bs58;
     use solana_sdk::signature::{Keypair, Signer};
+    use std::io::{self, BufRead};
 
     #[test]
     fn keygen() {
         let kp = Keypair::new();
-        println!("You've generated a new Solana wallet: {}", kp.pubkey().to_string());
+        println!(
+            "You've generated a new Solana wallet: {}",
+            kp.pubkey().to_string()
+        );
         println!("");
         println!("To save your wallet, copy and paste the following into a JSON file:");
 
@@ -13,9 +18,39 @@ mod tests {
     }
 
     #[test]
-    fn airdrop(){}
+    fn base58_to_wallet() {
+        println!("Input your private key as base58:");
+        let stdin = io::stdin();
+        let base58 = stdin.lock().lines().next().unwrap().unwrap();
+        println!("Your wallet file is: ");
+        let wallet = bs58::decode(base58).into_vec().unwrap();
+        println!("{:?}", wallet);
+    }
 
     #[test]
-    fn transfer_sol(){}
+    fn wallet_to_base58() {
+        println!("Input your private key as wallet file byte array:");
+        let stdin = io::stdin();
+        let wallet = stdin
+            .lock()
+            .lines()
+            .next()
+            .unwrap()
+            .unwrap()
+            .trim_start_matches('[')
+            .trim_end_matches(']')
+            .split(',')
+            .map(|s| s.trim().parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
 
+        println!("Your wallet file is: ");
+        let wallet = bs58::encode(wallet).into_string();
+        println!("{:?}", wallet);
+    }
+
+    #[test]
+    fn airdrop() {}
+
+    #[test]
+    fn transfer_sol() {}
 }
